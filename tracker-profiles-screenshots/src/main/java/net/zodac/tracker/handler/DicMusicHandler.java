56c0eaee837e74187df5121a7baceb760d20d1ba
zodac <arouge110@msn.com>
2025-03-10 13:25:18 +1300

    [DICMusic] Implemented tracker, with translation

---
+++ tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/DicMusicHandler.java
@@ -0,0 +1,89 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2021-2025 zodac.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package net.zodac.tracker.handler;
+
+import java.util.Collection;
+import java.util.List;
+import net.zodac.tracker.framework.TrackerHandler;
+import net.zodac.tracker.util.ScriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Implementation of {@link AbstractTrackerHandler} for the {@code DICMusic} tracker.
+ */
+@TrackerHandler(name = "DICMusic", needsManualInput = true, url = "https://dicmusic.com/")
+public class DicMusicHandler extends AbstractTrackerHandler {
+
+    /**
+     * Default constructor.
+     *
+     * @param driver      a {@link ChromeDriver} used to load web pages and perform UI actions
+     * @param trackerUrls the URLs to the tracker
+     */
+    public DicMusicHandler(final ChromeDriver driver, final Collection<String> trackerUrls) {
+        super(driver, trackerUrls);
+    }
+
+    @Override
+    public void navigateToLoginPage() {
+        final By loginLinkSelector = By.id("login-a");
+        final WebElement loginLink = driver.findElement(loginLinkSelector);
+        loginLink.click();
+        ScriptExecutor.waitForElementToAppear(driver, usernameFieldSelector(), DEFAULT_WAIT_FOR_PAGE_LOAD);
+    }
+
+    @Override
+    protected By loginButtonSelector() {
+        return By.id("login-btn");
+    }
+
+    @Override
+    protected By postLoginSelector() {
+        return By.id("userinfo");
+    }
+
+    @Override
+    protected By profilePageSelector() {
+        return By.xpath("//li[@id='nav_userinfo']/a[1]");
+    }
+
+    @Override
+    public Collection<By> getElementsPotentiallyContainingSensitiveInformation() {
+        return List.of(
+            By.tagName("a")
+        );
+    }
+
+    @Override
+    public boolean isNotEnglish() {
+        ScriptExecutor.translatePage(driver);
+        return true;
+    }
+
+    @Override
+    protected By logoutButtonSelector() {
+        // Highlight the profile menu to make the logout button interactable
+        final By logoutParentSelector = By.id("userinfo_username");
+        final WebElement logoutParent = driver.findElement(logoutParentSelector);
+        ScriptExecutor.moveTo(driver, logoutParent);
+
+        return By.xpath("//li[@id='nav_logout']/a[1]");
+    }
+}
