69fe3307a68384ce311a90ec32db14c761467715
zodac <arouge110@msn.com>
2025-03-11 00:26:05 +1300

    [Kufirc] Implemented tracker, with translation

---
+++ tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/KufircHandler.java
@@ -0,0 +1,130 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2021-2025 zodac.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package net.zodac.tracker.handler;
+
+import java.util.Collection;
+import java.util.List;
+import net.zodac.tracker.framework.TrackerHandler;
+import net.zodac.tracker.util.ScriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Implementation of {@link AbstractTrackerHandler} for the {@code Kufirc} tracker.
+ */
+@TrackerHandler(name = "Kufirc", needsManualInput = true, url = "https://kufirc.com/")
+public class KufircHandler extends AbstractTrackerHandler {
+
+    private static final String PASSKEY_PREFIX = "Passkey: ";
+
+    /**
+     * Default constructor.
+     *
+     * @param driver      a {@link ChromeDriver} used to load web pages and perform UI actions
+     * @param trackerUrls the URLs to the tracker
+     */
+    public KufircHandler(final ChromeDriver driver, final Collection<String> trackerUrls) {
+        super(driver, trackerUrls);
+    }
+
+    @Override
+    public void navigateToLoginPage() {
+        final By loginLinkSelector = By.xpath("//div[@id='logo']/ul[1]/li[2]/a[1]");
+        final WebElement loginLink = driver.findElement(loginLinkSelector);
+        loginLink.click();
+        ScriptExecutor.waitForElementToAppear(driver, usernameFieldSelector(), DEFAULT_WAIT_FOR_PAGE_LOAD);
+    }
+
+    @Override
+    protected By usernameFieldSelector() {
+        return By.xpath("//div[@id='username']/input[@name='username']");
+    }
+
+    @Override
+    protected By passwordFieldSelector() {
+        return By.xpath("//div[@id='password']/input[@name='password']");
+    }
+
+    @Override
+    public By loginButtonSelector() {
+        return By.id("login_button");
+    }
+
+    @Override
+    protected By postLoginSelector() {
+        return By.id("stats_block");
+    }
+
+    @Override
+    protected By profilePageSelector() {
+        return By.xpath("//a[@class='username']");
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * For {@link KufircHandler}, we also need to redact a passkey {@link WebElement}. We find an element with text that is prefixed by
+     * {@value #PASSKEY_PREFIX}, signifying a {@link WebElement} with a sensitive passkey. We redact this element by replacing all text with the
+     * prefix and {@value ScriptExecutor#DEFAULT_REDACTION_TEXT}.
+     *
+     * @see AbstractTrackerHandler#redactElements()
+     * @see ScriptExecutor#redactInnerTextOf(JavascriptExecutor, WebElement, String)
+     */
+    @Override
+    public int redactElements() {
+        final int superRedactedElements = super.redactElements();
+
+        final String passkeyRedactionText = PASSKEY_PREFIX + ScriptExecutor.DEFAULT_REDACTION_TEXT;
+        final WebElement passkeyElement = driver.findElement(By.xpath(String.format("//li[contains(text(),'%s')]", PASSKEY_PREFIX)));
+        ScriptExecutor.redactInnerTextOf(driver, passkeyElement, passkeyRedactionText);
+
+        return superRedactedElements + 1;
+    }
+
+    @Override
+    public Collection<By> getElementsPotentiallyContainingSensitiveInformation() {
+        return List.of(
+            By.tagName("a"),
+            By.tagName("span")
+        );
+    }
+
+    @Override
+    public boolean isNotEnglish() {
+        ScriptExecutor.translatePage(driver);
+        return true;
+    }
+
+    @Override
+    protected By logoutButtonSelector() {
+        // Highlight the profile menu to make the logout button interactable
+        final By logoutParentSelector = By.id("userinfo_username");
+        final WebElement logoutParent = driver.findElement(logoutParentSelector);
+        ScriptExecutor.moveTo(driver, logoutParent);
+
+        return By.xpath("//li[@id='nav_logout']/a[1]");
+    }
+
+    @Override
+    protected By postLogoutElementSelector() {
+        return By.xpath("//div[@id='logo']/ul[1]/li[2]/a[1]");
+    }
+}
