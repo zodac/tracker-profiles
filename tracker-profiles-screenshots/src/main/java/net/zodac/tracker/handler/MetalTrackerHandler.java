7ab7834f77b34789523f2b308b6b46ddc2ba5d1a
zodac <arouge110@msn.com>
2025-03-13 23:44:53 +1300

    [Metal-Tracker] Implemented tracker

---
+++ tracker-profiles-screenshots/src/main/java/net/zodac/tracker/handler/MetalTrackerHandler.java
@@ -0,0 +1,70 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2021-2025 zodac.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package net.zodac.tracker.handler;
+
+import java.util.Collection;
+import net.zodac.tracker.framework.TrackerHandler;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Implementation of {@link AbstractTrackerHandler} for the {@code Metal-Tracker} tracker.
+ */
+@TrackerHandler(name = "Metal-Tracker", needsManualInput = false, url = "https://en.metal-tracker.com/")
+public class MetalTrackerHandler extends AbstractTrackerHandler {
+
+    /**
+     * Default constructor.
+     *
+     * @param driver      a {@link ChromeDriver} used to load web pages and perform UI actions
+     * @param trackerUrls the URLs to the tracker
+     */
+    public MetalTrackerHandler(final ChromeDriver driver, final Collection<String> trackerUrls) {
+        super(driver, trackerUrls);
+    }
+
+    @Override
+    protected By usernameFieldSelector() {
+        return By.xpath("//input[@class='inputtext' and @name='username']");
+    }
+
+    @Override
+    protected By passwordFieldSelector() {
+        return By.xpath("//input[@class='inputtext' and @name='password']");
+    }
+
+    @Override
+    protected By loginButtonSelector() {
+        return By.xpath("//input[@type='submit' and @value='Enter']");
+    }
+
+    @Override
+    protected By postLoginSelector() {
+        return By.xpath("//ul[@class='user_data']");
+    }
+
+    @Override
+    protected By profilePageSelector() {
+        return By.xpath("//div[@class='user_block']//h4[@class='nickname']/span[1]/a[1]");
+    }
+
+    @Override
+    protected By logoutButtonSelector() {
+        return By.xpath("//div[@class='user_block']//div[@class='logout']/a[1]");
+    }
+}
