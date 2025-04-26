86637eefdf2c182cab568fc7be6f230f5b7c901d
zodac <arouge110@msn.com>
2025-04-26 18:20:42 +1200

    [CI] Updating Github release name and changelog content

--- .github/scripts/generate_changelog.sh
+++ .github/scripts/generate_changelog.sh
@@ -57,4 +57,4 @@ CHANGELOG_CONTENT=$(mktemp)
 } >> "${CHANGELOG_CONTENT}"
 
 # Output changelog content
-echo "changelog_content=${CHANGELOG_CONTENT}" >> "${GITHUB_ENV}"
+echo "changelog_content=$(cat "$CHANGELOG_CONTENT")" >> "${GITHUB_ENV}"
